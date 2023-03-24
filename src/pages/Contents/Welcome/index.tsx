56bf1697f685cc2333bbe7ffd5fd1378d146f5ff
Bgogoi123 <bgogoi055@gmail.com>
2023-03-24 11:49:52 +0530

    [feat][Landing Page] - Moving Cursor:   > Working on moving cursor for the welcome page.

--- src/pages/Contents/Welcome/index.tsx
+++ src/pages/Contents/Welcome/index.tsx
@@ -1,14 +1,15 @@
+import { useContext } from "react";
 import { Box, Flex } from "@mantine/core";
 import Lottie from "lottie-react";
-import { useContext, useEffect, useRef } from "react";
-import alarmClock from "../../../assets/lotties/alarm-clock.json";
-import artist from "../../../assets/lotties/artist.json";
+import { useEffect, useRef, useState } from "react";
+import notes from "../../../assets/lotties/notes.json";
 import coding from "../../../assets/lotties/coding.json";
 import network from "../../../assets/lotties/network.json";
-import notes from "../../../assets/lotties/notes.json";
 import reactLogo from "../../../assets/lotties/react-logo.json";
+import alarmClock from "../../../assets/lotties/alarm-clock.json";
+import digitalPaint from "../../../assets/lotties/digital-paint.json";
+import artist from "../../../assets/lotties/artist.json";
 
-import { MousePositionContext } from "../../../context";
 import "../../animations.css";
 import {
   alarmClockStyles,
@@ -19,37 +20,55 @@ import {
   reactLogoStyles,
   welcomTextContainer,
 } from "../styles";
+import { MousePositionContext } from "../../../context";
 
 const WelcomePage = () => {
   const { mousePosition, setMousePosition } = useContext(MousePositionContext);
+  const welcomeTextRef = useRef<any>();
   const containerRef = useRef<HTMLDivElement>(null);
   const cursorRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    welcomeTextRef.current.style.display = "none";
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("scroll", (event) => {
+      if (window.pageYOffset > 20) {
+        welcomeTextRef.current.style.display = "block";
+      } else {
+        welcomeTextRef.current.style.display = "none";
+      }
+    });
+  }, [window.pageYOffset]);
 
   useEffect(() => {
     const handleMouseMove = (event: any) => {
       setMousePosition([event.clientX, event.clientY]);
     };
+
     window.addEventListener("mousemove", handleMouseMove);
+
     return () => {
       window.removeEventListener("mousemove", handleMouseMove);
     };
   }, []);
 
-  // useEffect(() => {
-  //   console.log("x,y => ", mousePosition[0], mousePosition[1]);
-
-  //   const element = cursorRef.current;
-  //   if (!element) return;
-  //   element.style.top = `${mousePosition[1]}px`;
-  //   element.style.left = `${mousePosition[0]}px`;
-  // }, [mousePosition]);
+  useEffect(() => {
+    const element = cursorRef.current;
+    if (!element) return;
+    element.style.top = `${mousePosition[1]}px`;
+    element.style.left = `${mousePosition[0]}px`;
+  }, [mousePosition]);
 
   return (
     <Box ref={containerRef}>
       <div ref={cursorRef} className="movingCircle"></div>
       <Flex justify="center" align="center">
         <div style={welcomTextContainer}>
-          <b className="typing">Welcome</b>
+          <b className="typing" ref={welcomeTextRef}>
+            Welcome
+          </b>
         </div>
       </Flex>
 
--- src/pages/Contents/index.tsx
+++ src/pages/Contents/index.tsx
@@ -1,21 +1,27 @@
 import { Flex } from "@mantine/core";
+import { useState } from "react";
+import { MousePositionContext } from "../../context";
 import Profile from "./Profile";
 import Projects from "./Projects";
 import WelcomePage from "./Welcome";
 
 const Contents = () => {
+  const [mousePosition, setMousePosition] = useState<[number, number]>([0, 0]);
+
   return (
-    <Flex direction="column">
-      <div
-        style={{
-          height: "100vh",
-        }}
-      >
-        <WelcomePage />
-      </div>
-      <Projects />
-      <Profile />
-    </Flex>
+    <MousePositionContext.Provider value={{ mousePosition, setMousePosition }}>
+      <Flex direction="column">
+        <div
+          style={{
+            height: "100vh",
+          }}
+        >
+          <WelcomePage />
+        </div>
+        <Projects />
+        <Profile />
+      </Flex>
+    </MousePositionContext.Provider>
   );
 };
 
